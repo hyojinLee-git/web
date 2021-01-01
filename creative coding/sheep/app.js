8d67bdcfa62c307208ccccf508590126344fd6f8
hyojinLee-git <lily960310@naver.com>
2021-01-01 15:46:37 +0900

    sheep clone coding

--- creative coding/sheep/app.js
+++ creative coding/sheep/app.js
@@ -1,6 +1,14 @@
 import{
     Hill
-} from './hill.js';
+} from "./hill.js";
+
+import{
+    sheepController
+} from "./sheep-controller.js"
+
+import{
+    Sun
+} from './sun.js';
 
 class App{
     constructor(){
@@ -8,9 +16,15 @@ class App{
         this.ctx=this.canvas.getContext("2d");
         document.body.appendChild(this.canvas);
 
+        this.sun=new Sun();
+
         this.hills=[
+            new Hill('#fd6bea',0.2,12),
+            new Hill('#ff59c2',0.5,8),
             new Hill('#ff4674',1.4,6)
         ];
+
+        this.sheepController=new sheepController();
 
         window.addEventListener('resize',this.resize.bind(this),false);
         this.resize();
@@ -25,6 +39,14 @@ class App{
         this.canvas.width=this.stageWidth*2;
         this.canvas.height=this.stageHeight*2;
         this.ctx.scale(2,2);
+
+        this.sun.resize(this.stageWidth,this.stageHeight);
+
+        for (let i=0; i<this.hills.length;i++){
+            this.hills[i].resize(this.stageWidth,this.stageHeight);
+        }
+
+        this.sheepController.resize(this.stageWidth,this.stageHeight);
     }
     //canvas size 2배로 해서 선명한 해상도
 
@@ -33,6 +55,16 @@ class App{
         requestAnimationFrame(this.animate.bind(this));
 
         this.ctx.clearRect(0,0,this.stageWidth,this.stageHeight);
+
+        this.sun.draw(this.ctx,t);
+
+        let dots;
+        for (let i=0; i<this.hills.length;i++){
+            dots=this.hills[i].draw(this.ctx);
+        }
+
+        this.sheepController.draw(this.ctx,t,dots);
+
     }
 }
 
--- creative coding/sheep/hill.js
+++ creative coding/sheep/hill.js
@@ -15,11 +15,11 @@ export class Hill{
         this.gap=Math.ceil(this.stageWidth/(this.total-2));
         //stage보다 크게(양이 화면 밖에서 자연스럽게 걸어오도록)
 
-        for(let i=0; i<this.total;i++){
+        for(let i=0; i<this.total; i++){
             this.points[i]={
-                x:i*this.gap,
-                y:this.getY()
-            }
+                x : i * this.gap,
+                y : this.getY()
+            };
         }
     }
 
@@ -32,14 +32,25 @@ export class Hill{
         let prev=cur;
 
         let dots=[];
+        cur.x+=this.speed;
+
+        if(cur.x>-this.gap){
+            this.points.unshift({
+                x:-(this.gap*2),
+                y:this.getY()
+            });
+        } else if (cur.x>this.stageWidth+this.gap){
+            this.points.splice(-1);
+        }
 
         ctx.moveTo(cur.x,cur.y);
 
         let prevCx=cur.x;
         let prevCy=cur.y;
-
+    
         for (let i=1;i<this.points.length;i++){
             cur=this.points[i];
+            cur.x+=this.speed;
             
             const cx=(prev.x+cur.x)/2;
             const cy=(prev.y+cur.y)/2;
@@ -54,20 +65,26 @@ export class Hill{
                 y3:cy,
 
             });
+
+            prev=cur;
+            prevCx=cx;
+            prevCy=cy;
         }
         
         ctx.lineTo(prev.x,prev.y);
         ctx.lineTo(this.stageWidth,this.stageHeight);
         ctx.lineTo(this.points[0].x,this.stageHeight);
         ctx.fill();
+        
 
-        return dots
+        return dots;
     }
 
     //언덕의 Y값을 랜덤으로 주기
     getY(){
         const min=this.stageHeight/8;
         const max=this.stageHeight-min;
-        return min+Math.random()*max;
+
+        return min + Math.random() * max;
     }
 }

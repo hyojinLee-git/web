e2132e435d919523b099140662284c8529a9ee26
hyojinLee-git <lily960310@naver.com>
2021-04-08 17:56:42 +0900

    fast campus

--- fastcampus/javascript/index.js
+++ fastcampus/javascript/index.js
@@ -1,21 +1,4 @@
-function increaseAndPrint(n,callback){
-    setTimeout(()=>{
-        const increased=n+1;
-        console.log(increased);
-        if(callback){
-            callback(increased)
-        }
-    },1000)
-}
+async function main(){
 
-increaseAndPrint(0,n=>{
-    increaseAndPrint(n,n=>{
-        increaseAndPrint(n,n=>{
-            increaseAndPrint(n,n=>{
-                increaseAndPrint(n,n=>{
-                    console.log('작업 끝!');
-                })
-            })
-        })
-    })
-})
+}
+document.addEventListener('DOMContentLoaded', main);

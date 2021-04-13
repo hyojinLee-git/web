26036a542aafb0ed3fc3feab207e4a65d03665ca
hyojinLee-git <lily960310@naver.com>
2021-04-13 17:07:02 +0900

    jsClock

--- toy/timer/app.js
+++ toy/timer/app.js
@@ -1,40 +1,55 @@
 const time=document.querySelector('.time');
 const startbtn=document.querySelector('.startbtn');
 const stopbtn=document.querySelector('.stopbtn');
-
-
+const progress=document.querySelector('.now');
+const circle=document.querySelector('.progress-circle');
 
 //console.log(min, sec);
+function setMinSec(){
+    const val=time.innerText.split(':');
+    const min=val[0];
+    const sec=val[1];
+    let now_sec=parseInt(sec,10);
+    let now_min=parseInt(min,10);
+    now_sec+=1;
+    if (now_sec<10){
+        now_sec='0'+now_sec.toString();
+    }else{
+        if (now_sec>=60){
+            now_sec='00'
+            now_min+=1
+        }
+        now_sec=now_sec.toString();
+        now_min=now_min.toString();
+    }
+    if(now_min<10){
+        now_min='0'+now_min
+    } else if(now_min>=10){
+        now_min='00'
+        clearInterval(timer);
+    } 
+    time.innerText=`${now_min}:${now_sec}`;
+}
+
+//1초당 0.3픽셀
+progress.style.width='0px';
+let present_width=progress.offsetWidth;
+function setProgressBar(){
+    present_width+=1
+    progress.style.width=present_width+'px';
+}
 
 function startTimer(){
-    timer=setInterval(() => {   //var
-        const val=time.innerText.split(':');
-        let min=val[0];
-        let sec=val[1];
-        let now_sec=parseInt(sec,10);
-        let now_min=parseInt(min,10);
-        now_sec+=1;
-        if (now_sec<10){
-            now_sec='0'+now_sec.toString();
-        }else{
-            if (now_sec>=60){
-                now_sec='00'
-                now_min+=1
-            }
-            now_sec=now_sec.toString();
-            now_min=now_min.toString();
-        }
-        if(now_min<10){
-            now_min='0'+now_min
-        } else if(now_min>=60){
-            now_min='00'
-        } 
-        time.innerText=now_min+':'+now_sec;
-    }, 1000);
+    timer=setInterval(() => {
+        setMinSec()
+        setProgressBar()
+    }, 1000);  //var
 }
 function stopTimer(){
     clearInterval(timer);
-    console.log('stop');
 }
+
+
 startbtn.addEventListener('click',startTimer);
 stopbtn.addEventListener('click',stopTimer);
+
